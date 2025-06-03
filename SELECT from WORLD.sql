1d7fd8840c47b8af52fa7b9a075ca00d0fbc35ca
RAIMUNDO NONATO DE SOUSA <prof.raiworld@gmail.com>
2025-06-02 22:01:42 -0300

    Update SELECT from WORLD.sql
    add atv.

--- SELECT from WORLD.sql
+++ SELECT from WORLD.sql
@@ -1,1 +1,14 @@
+SELECT name, continent, population FROM world
+
+SELECT name FROM world WHERE population >= 200000000
+
+SELECT name, gdp/population FROM world
+WHERE population >= 200000000
+
+SELECT name, population/1000000  FROM world
+WHERE continent = 'South America'
+
+SELECT name, population  FROM world
+WHERE name = 'France' OR name = 'Germany' OR name = 'Italy'
+
 
